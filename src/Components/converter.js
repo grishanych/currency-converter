16add8a27d42d08c222ee6655e96094f58cd6037
Grygorii Nych <alsolo2011@gmail.com>
2022-02-14 22:35:25 +0200

    Create app currency converter, first commit

---
+++ src/Components/converter.js
@@ -0,0 +1,16 @@
+import React, { Component } from   'react';
+
+
+class Converter extends Component {
+
+    render() {
+
+        return(
+            <div>
+                <h1>Converter</h1>
+            </div>
+        )
+    }
+};
+
+export default Converter;
---
+++ src/Components/exchange.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Exchange extends Component {
+    render() {
+
+        return(
+            <div>
+                <h1>Current exchange rates</h1>
+            </div>
+        )
+    }
+};
+
+export default Exchange;
